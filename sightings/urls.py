704e64677406f4839ecf2ebadb70ff5da8b7b6a8
Yi Hu <yh3308@columbia.com>
2021-04-13 10:42:03 +0000

    Prettify stats template.

--- sightings/urls.py
+++ sightings/urls.py
@@ -3,7 +3,7 @@ from . import views
 app_name = 'sightings'
 
 urlpatterns=[
-        path('sightings/',views.index),
+        path('sightings/',views.index, name='index'),
         path('sightings/add/',views.add,name='add'),
         path('sightings/stats/', views.stats, name='stats'),
         path('sightings/<str:Unique_Squirrel_ID>/',views.update,name='update'),
