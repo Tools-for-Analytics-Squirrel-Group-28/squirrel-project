631ac0dfde588db9b74eb40f80d02f167aaf2660
Yifan Wang <yw3581@columbia.edu>
2021-04-13 08:56:03 +0000

    apply bootstrap template on index page

--- sightings/urls.py
+++ sightings/urls.py
@@ -5,10 +5,7 @@ app_name = 'sightings'
 urlpatterns=[
         path('sightings/',views.index),
         path('sightings/add/',views.add,name='add'),
-<<<<<<< HEAD
         path('map/',views.mapping,name='mapping'),
-=======
         path('sightings/stats/', views.stats, name='stats'),
->>>>>>> 895253159e20cf030ecdfd8673f8c09066299b6e
         path('sightings/<str:Unique_Squirrel_ID>/',views.update,name='update'),
 ]
